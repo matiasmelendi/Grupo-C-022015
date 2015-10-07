44b06108536f80cf4497d678b6810721527ea1c9
matiasmelendi <mmelendi@10pines.com>
2015-10-06 21:56:24 -0300

    * added players service and repository (not implemented yet)

---
+++ SuperGol/src/main/java/repositories/PlayersRepository.java
@@ -0,0 +1,18 @@
+package repositories;
+
+public class PlayersRepository {
+
+    /**
+    public List<Player> all(){
+        return null;
+    }
+
+    public void delete(Integer id){
+
+    }
+
+    public Player find(Integer id){
+        return null;
+    }
+    */
+}
