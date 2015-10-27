b15620d227b8dfec17fce030b25961d66ea3326e
matiasmelendi <mmelendi@10pines.com>
2015-10-27 20:03:30 -0300

    added two jasmine specs

--- SuperGol_Frontend/specs/test.js
+++ SuperGol_Frontend/specs/test.js
@@ -2,9 +2,19 @@ require('../resources/js/models/team.js');
 
 describe("Team tests :", function() {
 
-  it("a Team can be created", function() {
+  it("could be created", function(Team) {
     var team = new Team();
+
     expect(team).not.toBeNull();
+    expect(team).toBeDefined();
   });
+
+  it("should not have name and logo", function(Team) {
+    var team = new Team();
+
+    expect(team.name).toEqual('');
+    expect(team.logo).toEqual('');
+  });
+
 
 });
