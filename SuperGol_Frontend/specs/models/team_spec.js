c1ca7088a3997fadb28c9816e287fceb40c729ab
DavidCorrea <davidjonatan.c@gmail.com>
2015-11-10 13:24:16 -0300

    - Added Team Model tests.

--- SuperGol_Frontend/specs/models/team_spec.js
+++ SuperGol_Frontend/specs/models/team_spec.js
@@ -1,17 +1,50 @@
 describe("Team", function() {
 
-  it("could be created", function() {
-    var team = new Team();
+    var team;
+    var goalkeeper = new Player('David', 1);
 
-    expect(team).not.toBeNull();
-    expect(team).toBeDefined();
-  });
+    beforeEach(function() {
+        team = new Team();
+    });
 
-  it("should not have name and logo", function() {
-    var team = new Team();
+    it("can be created", function() {
+        expect(team).not.toBeNull();
+        expect(team).toBeDefined();
+    });
 
-    expect(team.name).toEqual('');
-    expect(team.logo).toEqual('');
-  });
+    it("should not have name and logo", function() {
+        expect(team.name).toEqual('');
+        expect(team.logo).toEqual('');
+    });
+
+    it("should not have any players when created", function() {
+        expect(team.players().length).toBe(0);
+    });
+
+    it("can add a goalkeeper when it doesn't have one", function() {
+        expect(team.canAddPlayer(goalkeeper)).toBe(true);
+    });
+
+    it("adds a goalkeeper when it doesn't have one", function() {
+        team.addPlayer(goalkeeper);
+        expect(team.players().length).toBe(1);
+    });
+
+    it("can't add a goalkeeper when it already has one", function() {
+        team.addPlayer(goalkeeper);
+        expect(team.canAddPlayer(goalkeeper)).toBe(false);
+
+        // Tries to add again, which is not possible.
+        team.addPlayer(goalkeeper);
+        expect(team.players().length).toBe(1);
+    });
+
+    it("can remove a player", function() {
+        team.addPlayer(goalkeeper);
+        expect(team.players().length).toBe(1);
+
+        team.removePlayer(goalkeeper);
+        expect(team.players().length).toBe(0);
+    });
 
 });
