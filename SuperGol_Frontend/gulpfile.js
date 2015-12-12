cd6d8e1dcade6b1013438d617d74dad1e49d28b8
DavidCorrea <davidjonatan.c@gmail.com>
2015-12-12 19:01:45 -0300

    - Travis Controllers Tests re-enabled; - Added Auth0 required files; - Re-organized HTML Imports.

--- SuperGol_Frontend/gulpfile.js
+++ SuperGol_Frontend/gulpfile.js
@@ -27,11 +27,15 @@ gulp.task('tests-angular', function () {
     var filesForTest = ['node_modules/angular/angular.min.js',
                         'node_modules/angular-mocks/angular-mocks.js',
                         'node_modules/angular-route/angular-route.min.js',
+                        'node_modules/angular-cookies/angular-cookies.min.js',
                         'node_modules/angular-translate/dist/angular-translate.min.js',
                         'node_modules/angular-utils-pagination/dirPagination.js',
+                        'node_modules/ng-file-upload/dist/ng-file-upload.min.js',
+                        'resources/js/imports/*.js',
                         'app.js',
                         'resources/js/models/*.js' ,
                         'resources/js/controllers/*.js',
+                        'resources/js/services/*.js',
                         'specs/angular/**/*_spec.js'];
     return gulp.src(filesForTest)
         .pipe(watch(filesForTest))
@@ -40,19 +44,21 @@ gulp.task('tests-angular', function () {
 });
 
 gulp.task('travis', function () {
-    /*
-        'node_modules/angular/angular.min.js',
-        'node_modules/angular-mocks/angular-mocks.js',
-        'node_modules/angular-route/angular-route.min.js',
-        'node_modules/angular-translate/dist/angular-translate.min.js',
-        'node_modules/angular-utils-pagination/dirPagination.js',
-        'app.js',
-        'resources/js/models/-.js' ,
-        'resources/js/controllers/-.js',
-        'specs/angular/--/-_spec.js
-    */
     var filesForTest = ['resources/js/models/*.js',
-                        'specs/models/*_spec.js'];
+                        'specs/models/*_spec.js',
+                        'node_modules/angular/angular.min.js',
+                        'node_modules/angular-mocks/angular-mocks.js',
+                        'node_modules/angular-route/angular-route.min.js',
+                        'node_modules/angular-cookies/angular-cookies.min.js',
+                        'node_modules/angular-translate/dist/angular-translate.min.js',
+                        'node_modules/angular-utils-pagination/dirPagination.js',
+                        'node_modules/ng-file-upload/dist/ng-file-upload.min.js',
+                        'resources/js/imports/*.js',
+                        'app.js',
+                        'resources/js/models/*.js' ,
+                        'resources/js/controllers/*.js',
+                        'resources/js/services/*.js',
+                        'specs/angular/**/*_spec.js'];
     return gulp.src(filesForTest)
         .pipe(jasmine({
             integration: true
--- SuperGol_Frontend/specs/angular/controllers/teamController_spec.js
+++ SuperGol_Frontend/specs/angular/controllers/teamController_spec.js
@@ -1,4 +1,4 @@
-describe('Team Controller', function() {
+describe('Team Creation Controller', function() {
 
     var rootScope, scope, controller;
 
@@ -7,13 +7,15 @@ describe('Team Controller', function() {
         inject(function($injector, $controller){
             rootScope = $injector.get('$rootScope');
             scope = rootScope.$new();
-            controller = $controller('TeamCtrl', {$scope: scope});
+            controller = $controller('TeamCreationCtrl', {$scope: scope});
         });
     });
 
     it('should be able to get the Team in scope correctly', function() {
         expect(scope.team).not.toBeNull();
     });
+
+    /* These tests should be in a file related to Services Calls tests.
 
     it('should be able to select a Player from Available List to Selected List', function() {
         var selectedPlayer = scope.allPlayers[0];
@@ -35,5 +37,7 @@ describe('Team Controller', function() {
         expect(scope.team.players().length).toBe(0);
         expect(scope.allPlayers.length).toBe(initialAvailablePlayersSize);
     });
+
+    */
 
 });
