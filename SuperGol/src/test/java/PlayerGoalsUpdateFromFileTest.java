5b0e89af63568eb33de557f356f10cf2dd016d0a
matiasmelendi <mmelendi@10pines.com>
2015-12-14 15:57:31 -0300

    * test fixed

--- SuperGol/src/test/java/PlayerGoalsUpdateFromFileTest.java
+++ SuperGol/src/test/java/PlayerGoalsUpdateFromFileTest.java
@@ -2,6 +2,8 @@ import exceptions.CouldNotUpdateAPreviousRound;
 import exceptions.FileHasNoHeaderID;
 import exceptions.UpdateGoalsFromFileFailure;
 import model.PlayerGoalsUpdateFromFile;
+import model.Reader;
+import org.junit.Before;
 import org.junit.Test;
 
 import java.io.File;
@@ -11,6 +13,12 @@ public class PlayerGoalsUpdateFromFileTest {
     private File invalidFile = new File("src/test/support/playersScore_NO_OK.csv");
     private File validFile = new File("src/test/support/playersScore_OK.csv");
     private File emptyFile = new File("src/test/support/playersScore_empty.csv");
+
+    @Before
+    public void setUp(){
+        Reader.assignPlayersProvider(new PlayersProviderForTest());
+    }
+
 
     @Test(expected = UpdateGoalsFromFileFailure.class)
     public void couldNotUpdateARoundWithAFileThatWasUsedPreviously() throws UpdateGoalsFromFileFailure, CouldNotUpdateAPreviousRound, FileHasNoHeaderID {
